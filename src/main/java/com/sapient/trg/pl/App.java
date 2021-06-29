9f8c064559815201b22a9f513a75c62f8d1671d1
Navneetdhar <navneet.dhar@gmail.com>
2021-06-29 16:38:23 +0530

    assessment committed in github

---
+++ src/main/java/com/sapient/trg/pl/App.java
@@ -0,0 +1,11 @@
+package com.sapient.trg.pl;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Week 1 Assesment Navneet Dhar");
+    }
+}
